e9fa02fa6a57191bd8eff126993e7defaf53fb51
Raquel araujo <163220125+Rackaraujo@users.noreply.github.com>
2024-04-11 12:01:54 -0300

    Add files via upload

---
+++ Exercicios/AlbumDeFotos/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AlbumDeFotos
+{
+    public class Program
+    {
+        static void Main()
+        {
+            AlbumDeFotos album = new AlbumDeFotos(10);
+            
+            Console.WriteLine($"Número total de fotos permitido: {album.NumeroTotalDeFotos}");
+
+            album.NumeroDeFotos = 15;
+            Console.WriteLine($"Número de fotos no álbum: {album.NumeroDeFotos}");
+
+            album.NumeroDeFotos = 25;
+            Console.WriteLine($"Número de fotos no álbum: {album.NumeroDeFotos}");
+        }
+    }
+}
