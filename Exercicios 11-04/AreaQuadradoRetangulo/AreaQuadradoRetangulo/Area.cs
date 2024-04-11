982da4f4b3fa3bebf0b0d14f69d4303ec35aade6
Raquel araujo <163220125+Rackaraujo@users.noreply.github.com>
2024-04-11 12:05:18 -0300

    Add files via upload

---
+++ Exercicios 11-04/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Area.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaQuadradoRetangulo
+{
+    public class Area
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+        public double AreaTotal { get; set; }
+
+        public string Calcular()
+        {
+            Console.WriteLine("Digite a base: ");
+            Base = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a altura: ");
+            Altura = Convert.ToDouble(Console.ReadLine());
+            AreaTotal = Base * Altura;
+            string tipo = "";
+
+            if(Base == Altura)
+            {
+                tipo = "quadrado";
+            }
+            else
+            {
+                tipo = "retângulo";
+            }
+
+            string resultado = "A área é " + AreaTotal;
+            resultado += "\nIsso é um " + tipo;
+
+            return resultado; 
+        }
+
+        public string Dados()
+        {
+            string informacoes = "O valor da base é " + Base;
+            informacoes += "\nO valor da altura é " + Altura;
+
+            return informacoes;
+        }
+    }
+}
