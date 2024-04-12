010fb21d04522944236d320430f5f29cd704ebac
Raquel araujo <163220125+Rackaraujo@users.noreply.github.com>
2024-04-12 11:55:44 -0300

    Add files via upload

---
+++ Exercicios 11-04/FolhaDePagamento/FolhaDePagamento/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FolhaDePagamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pagamento p = new Pagamento();
+            p.Nome = "Dave Mustaine";
+            p.Sal = 10000;
+            p.HED = 0;
+            p.HEN = 15;
+            p.ND = 0;
+            p.Fal = 13;
+            p.DE = 5;
+            p.REF = 5;
+            p.Val = 1;
+            Console.WriteLine(p.FolhaDePagamento());
+        }
+    }
+}
