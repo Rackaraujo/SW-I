b2a492574ec3008656bbc1e24aa05e589cf92695
Raquel araujo <163220125+Rackaraujo@users.noreply.github.com>
2024-04-12 11:38:16 -0300

    Add files via upload

---
+++ Exercicios 11-04/AlbumDeFotos/AlbumDeFotos/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AlbumDeFotos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número total de páginas do álbum:");
+            int numeroTotalDePaginas = Convert.ToInt32(Console.ReadLine());
+
+            Album album = new Album(numeroTotalDePaginas);
+
+            Console.WriteLine("Digite o número de fotos a serem adicionadas:");
+            int numeroDeFotos = Convert.ToInt32(Console.ReadLine());
+
+            album.NumeroDeFotos = numeroDeFotos;
+        }
+    }
+}
