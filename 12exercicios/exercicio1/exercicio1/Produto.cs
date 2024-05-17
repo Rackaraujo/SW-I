51661ac79363c06a3809aca396514fbf5ce703b0
Raquel araujo <163220125+Rackaraujo@users.noreply.github.com>
2024-05-17 18:47:05 -0300

    Add files via upload

---
+++ 12exercicios/exercicio1/exercicio1/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1
+{
+    public class Produto
+    {
+        // Atributos da classe
+        public string Nome { get; private set; }
+        public decimal Preco { get; private set; }
+        public int QuantidadeEmEstoque { get; private set; }
+
+        // Construtor para inicializar os atributos
+        public Produto(string nome, decimal preco, int quantidadeEmEstoque)
+        {
+            Nome = nome;
+            Preco = preco;
+            QuantidadeEmEstoque = quantidadeEmEstoque;
+        }
+
+        // Método para adicionar itens ao estoque
+        public void AdicionarEstoque(int quantidade)
+        {
+            QuantidadeEmEstoque += quantidade;
+        }
+
+        // Método para remover itens do estoque
+        public void RemoverEstoque(int quantidade)
+        {
+            QuantidadeEmEstoque -= quantidade;
+        }
+    }
+}
---
+++ 12exercicios/exercicio1/exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando uma instância de Produto
+            Produto produto = new Produto("Caneta", 1.50m, 100);
+
+            // Exibindo informações iniciais do produto
+            Console.WriteLine($"Produto: {produto.Nome}");
+            Console.WriteLine($"Preço: {produto.Preco}");
+            Console.WriteLine($"Quantidade em Estoque: {produto.QuantidadeEmEstoque}");
+
+            // Adicionando 20 itens ao estoque
+            produto.AdicionarEstoque(20);
+            Console.WriteLine($"Quantidade em Estoque após adição: {produto.QuantidadeEmEstoque}");
+
+            // Removendo 10 itens do estoque
+            produto.RemoverEstoque(10);
+            Console.WriteLine($"Quantidade em Estoque após remoção: {produto.QuantidadeEmEstoque}");
+        }
+    }
+}
