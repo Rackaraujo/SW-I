e2ac557f73e8a081682daec8bf5d79ba6342d01c
Raquel araujo <163220125+Rackaraujo@users.noreply.github.com>
2024-05-17 18:59:08 -0300

    Add files via upload

---
+++ exercicio10/exercicio10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cachorro = new Animal("Rex", "Cachorro", 5);
+            cachorro.EmitirSom();  // Output: Rex, que é um(a) Cachorro, está emitindo um som.
+
+            Animal gato = new Animal("Mimi", "Gato", 3);
+            gato.EmitirSom();  // Output: Mimi, que é um(a) Gato, está emitindo um som.
+
+            Animal vaca = new Animal("Mimosa", "Vaca", 7);
+            vaca.EmitirSom();  // Output: Mimosa, que é um(a) Vaca, está emitindo um som.
+        }
+    }
+}
